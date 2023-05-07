a30de62333e00f624ee42d7076de0f8cff09220d
mrmps <41216139+mrmps@users.noreply.github.com>
2023-05-07 02:29:50 -0700

    Update train.py

--- Efficient_RedPajama_Finetuning/train.py
+++ Efficient_RedPajama_Finetuning/train.py
@@ -21,7 +21,7 @@ os.environ["TOKENIZERS_PARALLELISM"] = "true"
 ### This dataclass is used to store the configuration for the trainer.
 @dataclass
 class TrainerConfig:
-    batch_size: int = 12
+    batch_size: int = 2
     micro_batch_size: int = 3
     lora_r: int = 8  #  This controls the rank of the matrices used in LoRA
     lora_alpha: int = 16 # Hyperparameter for LoRA training
